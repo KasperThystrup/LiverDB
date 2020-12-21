74bf0bf7737df4b34caff5712ee41eba2d5eaf60
Kasper Thystrup Karstensen <kathka@gmail.com>
2020-12-21 13:30:17 +0100

    Metadata rule to script
    Resolved the issue with aggregating the Metadata rule, regarding the {sample} widlcard in params. Currently the solution is a for loop, iterating over the  list oject.

---
+++ workflow/scripts/entrez_direct.py
@@ -0,0 +1,10 @@
+from os.path import dirname
+from snakemake import shell
+
+email = snakemake.params[0]
+samples = snakemake.params[1]
+output_dir = snakemake.params[2]
+
+for sample in samples:
+	metadata_file = output_dir + sample + ".csv"
+	shell("esearch -db sra -query %s -email %s | efetch -format runinfo > %s" %(sample, email, metadata_file))

ea0a5ab8f2472ae7c4bfda6be0ee40252819690b
Kasper Thystrup Karstensen <kathka@gmail.com>
2021-01-12 12:32:35 +0100

    P02: Fixed script input error for fasterq-dump.py

--- workflow/scripts/fasterq_dump.py
+++ workflow/scripts/fasterq_dump.py
@@ -5,8 +5,9 @@ from snakemake import shell
 metadata_file = snakemake.input[0]
 sra_file = snakemake.input[1]
 cmd = snakemake.params[0]
-fastq_1 = snakemake.output[0]
-fastq_2 = snakemake.output[1]
+fastq = snakemake.output[0]
+fastq_1 = snakemake.output[1]
+fastq_2 = snakemake.output[2]
 threads = snakemake.threads
 
 
@@ -17,23 +18,21 @@ layout = meta["LibraryLayout"][0]
 
 fasterq_dump_str = " --force --split-files --temp %s --threads %s --outdir %s %s"
 
-sample_id = basename(sra_file).replace("\.sra", "")
-
 temp_dir = dirname(fastq_1)
 if layout == "SINGLE":
-	fq = sample_id + ".fastq"
 	dummy_com = " ; mv %s %s ; touch %s"
 	fasterq_dump_str += dummy_com
-	fasterq_dump = cmd + fasterq_dump_str %(temp_dir, threads, temp_dir, sra_file, fq, fastq_1, fastq_2)
+	fasterq_dump = cmd + fasterq_dump_str %(temp_dir, threads, temp_dir, sra_file, fastq, fastq_1, fastq_2)
 elif layout != "PAIRED":
 	exit("fasterq_dump: Layout type could not be interpreted; %s" %layout)
-else:
-	fasterq_dump = cmd + fasterq_dump_str %(temp_dir, threads, temp_dir, sra_file)
+
+fasterq_dump = cmd + fasterq_dump_str %(temp_dir, threads, temp_dir, sra_file)
 
 print("DEBUG:" + fasterq_dump)
 shell(fasterq_dump)
 
-if not exists(fastq_1):
+print(layout, exists(fastq_1))
+if layout == "PAIRED" and not exists(fastq_1):
 	sample_id = basename(sra_file).replace("\.sra", "")
 	print("Paired end dataset:", id, "")
 
